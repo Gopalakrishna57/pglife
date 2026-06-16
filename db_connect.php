a143a6e16c7dc65a37139e2dd1e48e1247b82e34
gopalakrishna <gunjegopalakrishna77@gmail.com>
2026-06-16 17:27:14 +0530

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,12 +1,14 @@
 <?php
-$db_hostname = "sql307.byetcluster.com";
-$db_username = "if0_42188214";
-$db_password = "WYHSJP8r1l2";
-$db_name = "if0_42188214_pglife";
+$db_host = "127.0.0.1";
+$db_user = "root";
+$db_pass = "";
+$db_name = "pglife";
 
-$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if (!$conn) {
-    echo "Connection failed: " . mysqli_connect_error();
+if (mysqli_connect_error()) {
+    $response = array("success" => false, "message" => "Database Connection Failed: " . mysqli_connect_error());
+    echo json_encode($response);
     exit;
 }
+?>
